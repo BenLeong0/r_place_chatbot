3cfd02021fd3d17a499caad3998a373df2c7bd83
benleong0 <benleong0@gmail.com>
2022-07-17 16:21:42 +0100

    basic setup

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -7,13 +7,23 @@ const server = app.listen(5000, () => console.log("Server running"));
 
 const wss = new WebSocket.Server({ server });
 
+const broadcastMessage = (message: string) => {
+    wss.clients.forEach((client) => {
+        client.send(message);
+    });
+}
+
 wss.on("connection", (ws: WebSocket) => {
     console.log("A new client connected");
     ws.send("welcome!");
 
     ws.on("message", (message) => {
-        console.log(`Received message: ${message}`)
-        ws.send(`Received your message :)`)
+        console.log(`Received message: ${message}`);
+        broadcastMessage(`Received a message: ${message}`);
+    })
+
+    ws.on("close", () => {
+        console.log("client disconnected")
     })
 })
 
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const socket = new WebSocket('ws://localhost:5000');
+
+    socket.addEventListener('open', (event) => {
+        console.log("Connected to server")
+    });
+
+    socket.addEventListener('message', (event) => {
+        console.log("Message from server:", event.data)
+    })
+
+    const sendMessage = (message: string) => {
+        socket.send(message)
+    }
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <button onClick={() => {sendMessage("hi")}}>send "hi" to the server</button>
+            </header>
+        </div>
+    );
 }
 
 export default App;
